ce83c04610521c7f26f45377c803252fc14fcdae
cj <cj6209577@sina.com>
2020-06-12 21:56:45 +0800

    404页面

---
+++ src/views/404.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="not-page">
+    <h1>404 not found</h1>
+    <router-link to="/" >返回首页</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+  .not-page{
+    text-align: center;
+    h1{
+      padding: 40px;
+    }
+    a{
+      font-size: 24px;
+    }
+  }
+</style>
