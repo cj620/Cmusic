e35fa1f89cbe2a207afe9f5e298b394cdd15f941
cj <cj6209577@sina.com>
2020-06-14 15:31:15 +0800

    searchBar bug

--- src/components/Tab/TabSearch.vue
+++ src/components/Tab/TabSearch.vue
@@ -1,15 +1,32 @@
 <template>
   <div>
-<searchBar />
+    <searchBar :keyword="keyword" />
   </div>
 </template>
 
 <script>
 import searchBar from '@/components/Tab/components/searchBar/index'
+import { getSearchMultimatch, getSearchMusic } from '@/api/api.js'
   export default {
     components:{
       searchBar
-    }
+    },
+    data() {
+      return {
+        keyword:'',
+        songList:[],
+        isShowSongList:false
+      }
+    },
+    // methods:{
+    //   searchMusic(params){
+    //     this.keyword = params.keywords
+    //     Promise.all([getSearchMusic(params),getSearchMultimatch(params)]).then(res =>{
+    //       console.log(res);
+          
+    //     })
+    //   }
+    // }
   }
 </script>
 
--- src/components/Tab/components/searchBar/index.vue
+++ src/components/Tab/components/searchBar/index.vue
@@ -25,7 +25,7 @@
       }
     },
     watch:{
-      keyword:function(newVal){
+      keyword:function(newVal){   //对keyword进行监听，将新值传给text
         this.text = newVal
       }
     },
@@ -45,5 +45,38 @@
 </script>
 
 <style lang="scss" scoped>
-
+.search-bar {
+  height: 60px;
+  border-bottom: 1px solid #f3f2f2;
+  form {
+    padding: 16px 10px;
+    color: #666;
+    .inputcover {
+      position: relative;
+      width: 100%;
+      height: 30px;
+      padding: 0 30px;
+      box-sizing: border-box;
+      background: #ebecec;
+      border-radius: 30px;
+      .van-icon {
+        position: absolute;
+        left: 0;
+        top: 7px;
+        margin: 0 8px;
+        font-size: 16px;
+        vertical-align: middle;
+      }
+      input {
+        width: 100%;
+        height: 30px;
+        line-height: 30px;
+        font-size: 14px;
+        background: #ebecec;
+        border: none;
+        font-weight: 500;
+      }
+    }
+  }
+}
 </style>
